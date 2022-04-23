bf76a985b52d5de9c7538de58a35d98ad55c108b
Michael Lechowicz <ichal.lechowicz@gmail.com>
2022-04-23 19:01:19 +0200

    Implement routing

--- index.php
+++ index.php
@@ -1,71 +1,11 @@
 <?php
 
-// echo 'Hi there 👋 from Michael.';
+require 'Routing.php';
 
-$data = ['this is a simple string', 'orange'];
-$hasString = "test string";
+$path = trim($_SERVER['REQUEST_URI'], '/');
 
+$path = parse_url($path, PHP_URL_PATH);
 
-echo md5($hasString);
-
-$data_assoc = [
-    'apple' => ['green', 'red'],
-    'orange' => 'orange',
-    'cherry' => 'red'
-];
-
-
-$ary = array("1"=>'One','Two',"3"=>'Three');
-$a = '1'; $b = count($ary);
-while ($a <= $b) {
- $pr = $ary[$a];
- print $a." has value ". "$pr<br>";
- $a++;
-}
-
-
-class Pizza {
-    private $radius;
-
-
-    function __construct($radius = 0) {
-        $this->radius = $radius;
-    }
-
-    function getRadius(){
-        return $this->radius;
-    }
-
-}
-
-$object = new Pizza(40);
-
-echo Pizza::class;
-
-echo "We ordered pizza with radius " . $object->getRadius();
-
-// echo $data . 4;
-
-// var_dump($data);
-
-// die($data);
-
-// var_dump($data);
-
-
-$kolor="red";
-ECHO "Tekst HTML<br>";
-echo "Tekst HTML<br>";
-EcHo "Tekst HTML<br>";
-
-// phpinfo();
-
-echo '2' == 2 ? 'true' : 'false';
-echo '<br/>';
-echo '2' === 2 ? 'true' : 'false';
-
-$test = null;
-echo '<br/>';
-echo $test ?: 'is not defined';
-
-
+Routing::get('index', 'DefaultController');
+Routing::get('projects', 'DefaultController');
+Routing::run($path);
---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'AppController.php';
+
+class DefaultController extends AppController{
+    public function index(){
+        // TODO display login.html
+        $this->render('enter-page/login'); 
+    }
+
+    public function projects() {
+        // TODO display projects.html
+        $this->render('dashboard');
+    }
+}
