a5fadfcd707af162e01915e556949729c77d363e
Michael Lechowicz <ichal.lechowicz@gmail.com>
2022-04-26 18:25:47 +0200

    Delete logs

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ require_once 'Router.php';
 
 $path = trim($_SERVER['REQUEST_URI'], '/');
 
-var_dump($path);
-die("To jest dramat!");
+// var_dump($path);
+
 $path = parse_url($path, PHP_URL_PATH);
 
 Router::get('', 'DefaultController');
