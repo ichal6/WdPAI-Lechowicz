0f420775064abf43ad0bee99445ea022f27897ec
Michael Lechowicz <ichal.lechowicz@gmail.com>
2022-06-17 18:16:19 +0200

    Move header to separate file

---
+++ public/views/portal/commons/header.php
@@ -0,0 +1,25 @@
+<?php
+echo    '<header>
+        <a href="/dashboard"><img src="public/assets/logo-full-portal.svg" alt="logo"></a>
+        <div id="search-bar">
+            <img src="public/assets/search-icon.svg" alt="Search icon">
+            <input placeholder="Type here for search">
+        </div>
+        <div id="name">
+            <a href="/account">';
+         if(isset($messages)){
+                    foreach ($messages as $message){
+                        echo $message;
+                    }
+                } else{
+                echo "Jan Przykładowy";
+                }
+    
+echo    '<img src="public/assets/account-icon.svg" alt="account icon"></a>
+        </div>
+        <div id="log-out">
+            <a href="/logout">Log out<img src="public/assets/log-out-icon.svg" alt="logout icon"></a>
+        </div>
+    </header>';
+
+?>
--- public/views/portal/lists.php
+++ public/views/portal/lists.php
@@ -10,27 +10,9 @@
     <title>Lists</title>
 </head>
 <body>
-    <header>
-        <a href="/dashboard"><img src="public/assets/logo-full-portal.svg" alt="logo"></a>
-        <div id="search-bar">
-            <img src="public/assets/search-icon.svg" alt="Search icon">
-            <input placeholder="Type here for search">
-        </div>
-        <div id="name">
-            <a href="/account">                <?php if(isset($messages)){
-                    foreach ($messages as $message){
-                        echo $message;
-                    }
-                } else{
-                echo "Jan Przykładowy";
-                }
-
-                ?><img src="public/assets/account-icon.svg" alt="account icon"></a>
-        </div>
-        <div id="log-out">
-            <a href="/logout">Log out<img src="public/assets/log-out-icon.svg" alt="logout icon"></a>
-        </div>
-    </header>
+    <?php
+        include_once('commons/header.php');
+    ?>
 
     <div id="main-part">
         <nav id="left-menu">
