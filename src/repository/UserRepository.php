3e88f9c2ebf1a37ffbb589f0637865c0fc81d9cc
Michael Lechowicz <ichal.lechowicz@gmail.com>
2022-06-17 15:07:10 +0200

    Add edit option for user

--- src/repository/UserRepository.php
+++ src/repository/UserRepository.php
@@ -74,4 +74,67 @@ class UserRepository extends Repository
         $stmt->bindParam(':id', $id, PDO::PARAM_STR);
         $stmt->execute();
     }
+
+    public function editUser(string $oldEmail, User $updateUser){
+        $pdo = $this->database->connect();
+        try {
+            $stmt = $this->database->connect()->prepare('
+            SELECT u.id, id_user_details, email, password, name, surname FROM users u LEFT JOIN user_details ud 
+            ON u.id_user_details = ud.id WHERE email = :email
+        ');
+            $stmt->bindParam(':email', $oldEmail, PDO::PARAM_STR);
+            $stmt->execute();
+
+            $oldUser = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            $pdo->beginTransaction();
+            $stmt = $pdo->prepare('
+                UPDATE user_details SET
+                name = :name, surname = :surname
+                WHERE id=:id
+            ');
+
+            print $oldUser['name'].
+                $oldUser['surname'].
+                $oldUser['id_user_details'];
+
+            $stmt->execute([
+                $updateUser->getName(),
+                $updateUser->getSurname(),
+                $oldUser['id_user_details']
+            ]);
+
+            $stmt = $this->database->connect()->prepare('
+                                UPDATE users SET
+                email = :email, password = :password
+                WHERE id=:id
+            ');
+            $stmt->execute([
+                $updateUser->getEmail(),
+                $updateUser->getPassword(),
+                $oldUser['id']
+            ]);
+            $pdo->commit();
+        } catch (PDOException $ex){
+            $pdo->rollBack();
+            throw $ex;
+        }
+    }
+
+    public function removeUser(string $email){
+
+    }
+
+    private function getUserDetailsID(string $email): int
+    {
+        $stmt = $this->database->connect()->prepare('
+            SELECT id_user_details FROM users WHERE email = :email
+        ');
+        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+        $stmt->execute();
+
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $user['id'];
+    }
 }
