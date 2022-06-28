6a196346da31b781c63dc210fc5447b85f881591
Michael Lechowicz <ichal.lechowicz@gmail.com>
2022-06-28 13:51:46 +0200

    Move JS codes to separate file

---
+++ public/js/enter-page.js
@@ -0,0 +1,5 @@
+const mainIcon = document.getElementById('about-item');
+
+mainIcon.addEventListener('click', function (event){
+    alert('With this application you can create shopping lists and add product to this list.');
+});
